8a50b290e275157674af9edb09df70d7960a2dca
Krish Soni <143406295+krish2248@users.noreply.github.com>
2025-02-06 22:44:34 +0530

    Update test.js

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -2,3 +2,4 @@ console.log("krish")
 
 console.log("hello this is my first Github repo")
 console.log("very excited")
+console.log("i m going to be very consistent and skillful in front-end development")
