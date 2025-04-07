5b6fc49547a8a6c06aeb8cf70eba9cc933ba442b
Krish Soni <143406295+krish2248@users.noreply.github.com>
2025-04-08 01:25:37 +0530

    Delete 01_basics directory

--- 01_basics/test.js
+++
@@ -1,5 +0,0 @@
-console.log("krish")
-
-console.log("hello this is my first Github repo")
-console.log("very excited")
-console.log("i m going to be very consistent and skillful in front-end development")
